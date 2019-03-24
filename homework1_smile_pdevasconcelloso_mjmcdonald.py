6f1514e3aaaadd0b65795d35011f73dd37ae82ce
Pedro Oporto <pdevasconcelloso@wpi.edu>
2019-03-24 23:56:01 +0000

    Added files for HW2

--- homework1_smile_pdevasconcelloso_mjmcdonald.py
+++ homework1_smile_pdevasconcelloso_mjmcdonald.py
@@ -15,6 +15,10 @@ class Predictor(object):
 
     def __repr__(self):
         return self.tuple().__str__()
+        
+class Image(object):
+    def __init__(self, grid):
+        self.grid = grid
 
 def fPC(y, yhat):
     misses = np.nonzero(y - yhat)
@@ -22,31 +26,34 @@ def fPC(y, yhat):
 
 
 def predict_per_predictor(predictor, image):
-    r1, c1, r2, c2 = predictor
-    bool = image[r1][c1] > image[r2][c2]
+    r1, c1, r2, c2 = predictor.tuple()
+    print(r1, c1, r2, c2)
+    print(image)
+    bool = image.grid[r1][c1] > image.grid[r2][c2]
     return 1 if bool else 0
 
 
 def predict_image(image, predictors):
-    image = image.reshape(6, 6)
-    # vectorized_predictor = np.vectorize(predict_per_predictor)
-    # predicts = vectorized_predictor(np.array(predictors), image)
-    predicts = np.apply_along_axis(lambda pred: predict_per_predictor(pred, image), 1, np.array(predictors))
+    vectorized_predictor = np.vectorize(predict_per_predictor)
+    vectorized_predictor.excluded.add(1)
+    predicts = vectorized_predictor(np.array(predictors), image)
+    #predicts = np.apply_along_axis(lambda pred: predict_per_predictor(pred, image), 1, np.array(predictors))
     avg_prediction = np.mean(predicts)
     return 1 if avg_prediction > 0.5 else 0
 
 
 def measureAccuracyOfPredictors(predictor, X, y, previous_predictors):
-    predictor = list(predictor)
-    if predictor in previous_predictors or (predictor[0] == predictor[2] and predictor[1] == predictor[3]):
+    
+    if predictor in previous_predictors or (predictor.r1 == predictor.r2 and predictor.c1 == predictor.c2):
         return float(0)
-    # vectorized_predict = np.vectorize(predict_image)
-    # vectorized_predict.excluded.add(1)
+    vectorized_predict = np.vectorize(predict_image)
+    vectorized_predict.excluded.add(1)
+    
     predictors = previous_predictors.copy()
     predictors.append(predictor)
     
-    yhat = np.apply_along_axis(lambda img: predict_image(img, predictors), 1, X)
-    # yhat = vectorized_predict(X, previous_predictors)
+    #yhat = np.apply_along_axis(lambda img: predict_image(img, predictors), 1, X)
+    yhat = vectorized_predict(X, previous_predictors)
     acc = fPC(y, yhat)
     return acc
 
@@ -69,24 +76,26 @@ def display_predictors(im, predictors):
 
 
 def stepwiseRegression(trainingFaces, trainingLabels):
+    face_objs = np.apply_along_axis(
+            lambda im: Image(im),
+            1, trainingFaces)
     possible_features = np.array(
         np.meshgrid(np.arange(6), np.arange(6), np.arange(6), np.arange(6))).T.reshape(-1, 4)
     # possible_features = np.array(list(map(lambda x: Predictor(x[0],x[1],x[2],x[3]), possible_features)))
-
+    feature_objs = np.apply_along_axis(
+            lambda pred: Predictor(pred[0], pred[1], pred[2], pred[3]),
+            1, possible_features)
     best_predictors = []
-    # vectorized_accuracy = np.vectorize(measureAccuracyOfPredictors)
-    # vectorized_accuracy.excluded.add(1)
-    # vectorized_accuracy.excluded.add(2)
-    # vectorized_accuracy.excluded.add(3)
+    vectorized_accuracy = np.vectorize(measureAccuracyOfPredictors)
+    vectorized_accuracy.excluded.add(1)
+    vectorized_accuracy.excluded.add(2)
+    vectorized_accuracy.excluded.add(3)
 
     for j in range(5):
-        predictor_results = np.apply_along_axis(
-            lambda pred: measureAccuracyOfPredictors(pred, trainingFaces, trainingLabels, best_predictors),
-            1, possible_features)
-        # predictor_results = vectorized_accuracy(possible_features, trainingFaces, trainingLabels, best_predictors)
+        predictor_results = vectorized_accuracy(feature_objs, face_objs, trainingLabels, best_predictors)
         
-        best_feature = possible_features[np.argmax(predictor_results)]
-        best_predictors.append(list(best_feature))
+        best_feature = feature_objs[np.argmax(predictor_results)]
+        best_predictors.append(best_feature)
         print("added", best_feature, "after round", j, "with acc ", predictor_results[np.argmax(predictor_results)])
     
     return best_predictors
@@ -102,5 +111,5 @@ def loadData(which):
 if __name__ == "__main__":
     testingFaces, testingLabels = loadData("test")
     trainingFaces, trainingLabels = loadData("train")
-    training_results = stepwiseRegression(trainingFaces[:100], trainingLabels[:100])
+    training_results = stepwiseRegression(trainingFaces[:10], trainingLabels[:10])
     print(training_results)
