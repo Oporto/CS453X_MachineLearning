944cae1a35fa18e5887739b61fb1b1a8e51f86c2
mattmcd25 <mattmcd25@gmail.com>
2019-03-31 22:42:08 -0400

    Work on gradient function

--- hw3/homework3_pdevasconcelloso_mjmcdonald.py
+++ hw3/homework3_pdevasconcelloso_mjmcdonald.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
+import sys
 
 
 def find_cost(predict, actual):
@@ -24,14 +26,10 @@ def calc_prediction(X, aug_w):
 
     
 def gradient(X, y, w, b):
-    diff = X.dot(w) + b - y
-    return np.transpose(X).dot(np.transpose(diff)) / X.shape[0], np.average(diff)
-
-
-def gradient_regularized(X, y, w, b, alpha):
-    diff = X.dot(w) + b - y
-    unregularized = np.transpose(X).dot(np.transpose(diff)) / X.shape[0]
-    return unregularized + (alpha / X.shape[0])*w, np.average(diff)
+    y_hat = calc_prediction(augment(X), np.hstack((w, b)))
+    gradient = X * (y_hat - y) / X.shape[0]
+    delta = np.average(gradient)
+    return gradient, delta
 
 
 def predicts(train_images, test_images, train_values, test_values, aug_w, name):
@@ -50,100 +48,45 @@ def predicts(train_images, test_images, train_values, test_values, aug_w, name):
     plt.title(name)
     plt.show()
     return train_pred, test_pred
-
-
-def predicts_regularized(train_images, test_images, train_values, test_values, aug_w, alpha):
-    train_pred = calc_prediction(train_images, aug_w)
-    test_pred = calc_prediction(test_images, aug_w)
-    train_cost = find_cost_with_penalty(train_pred, train_values, aug_w, alpha)
-    test_cost = find_cost_with_penalty(test_pred, test_values, aug_w, alpha)
-    np.save("regularized", aug_w)
-    print("Regularized Gradient Descent: \n")
-    print("Train cost: ", train_cost)
-    print("Test cost: ", test_cost)
-    
-    print("Mean square error: ", find_rmse(test_pred, test_values))
-
-    im = aug_w[0:-1]
-    im = im.reshape(48, 48)
-    plt.imshow(im, cmap='gray')
-    plt.title("regularized")
-    plt.show()
-    return train_pred, test_pred
     
 
 def augment(X):
     return np.hstack((X, np.transpose(np.atleast_2d(np.ones(X.shape[0])))))
 
 
-def one_shot(train_images, test_images, train_values, test_values):
-    aug_train_images = augment(train_images)
-    A = np.transpose(aug_train_images).dot(aug_train_images)
-    b = np.transpose(aug_train_images).dot(np.transpose(train_values))
-    aug_w = np.linalg.solve(A,b)
-    return predicts(train_images, test_images, train_values, test_values, aug_w, "one_shot")
-
-
-def gradient_descent(T, e, train_images, test_images, train_values, test_values):
+def stochastic_gradient_descent(epochs, batch_size, epsilon, train_images, test_images, train_values, test_values):
     aug_w = generate_weights()
     w = aug_w[:-1]
     b = aug_w[-1]
-    for t in range(T):
-        
-        g, delta = gradient(train_images, train_values, w, b)
-        w = w - e*g
-        b = b - e*delta
-        
-    aug_w = np.hstack((w,b))
-    return predicts(train_images, test_images, train_values, test_values, aug_w, "gradient_descent")
+    N = train_images.shape[0]
 
+    train_fused = np.hstack((train_images, train_values))
+    np.random.shuffle(train_fused)
+    train_images = train_fused[:,:-10]
+    train_values = train_fused[:,-10:]
 
-def gradient_descent_regularized(T, e, alpha, train_images, test_images, train_values, test_values):
-    aug_w = generate_weights()
-    #record_w = np.empty([500,2304])
-    #deltas = np.empty([500,1])
-    w = aug_w[:-1]
-    b = aug_w[-1]
-    for t in range(T):
-        g, delta = gradient_regularized(train_images, train_values, w, b, alpha)
-        '''if (t + 1) % 10 == 0 or t == 0:
-            record_w[(t+1)//10] = w
-            deltas[(t+1)//10] = delta
-            print(np.average(g), delta)'''
-        w = w - e * g
-        b = b - e * delta
-        
-    #np.save("weights_reg", record_w)
-    #np.save("deltas_reg", deltas)
-    
+    for epoch in range(epochs):
+        for round in range(math.ceil(N / batch_size)):
+            sample_img = train_images[round*batch_size:(round+1)*batch_size]
+            sample_val = train_values[round*batch_size:(round+1)*batch_size]
+
+            g, delta = gradient(sample_img, sample_val, w, b)
+            w = w - epsilon*g
+            b = b - epsilon*delta
+
     aug_w = np.hstack((w, b))
-    return predicts_regularized(train_images, test_images, train_values, test_values, aug_w, alpha)
+    return predicts(train_images, test_images, train_values, test_values, aug_w)
 
 
 def generate_weights():
     sigma = 0.01 ** 0.5
     mu = 0.5
-    return sigma * np.random.randn(48*48+1) + mu
-
-
-def show_five_worst(pred_values, test_values, test_images):
-    for i in range(5):
-        error = np.abs(test_values - pred_values)
-        index = np.argmax(error)
-        im = test_images[index]
-        im = im.reshape(48, 48)
-        plt.imshow(im, cmap='gray')
-        plt.title("worst #"+str(i+1)+": predicted "+str(pred_values[index])+" but was "+str(test_values[index]))
-        plt.show()
-        pred_values[index] = test_values[index] # so it is ignored next time
+    return sigma * np.random.randn(24*24+1) + mu
 
 
 if __name__ == "__main__":
-    train_images = np.load("age_Xtr.npy").reshape(-1, 2304)
-    test_images = np.load("age_Xte.npy").reshape(-1, 2304)
-    train_values = np.load("age_ytr.npy")
-    test_values  = np.load("age_yte.npy")
-    # m1train, m1test = one_shot(train_images, test_images, train_values, test_values)
-    # m2train, m2test = gradient_descent(5000, 0.003, train_images, test_images, train_values, test_values)
-    m3train, m3test = gradient_descent_regularized(5000, 0.003, 0.1, train_images, test_images, train_values, test_values)
-    show_five_worst(m3test, test_values, test_images)
+    train_images = np.load("small_mnist_train_images.npy").reshape(-1, 784)
+    test_images = np.load("small_mnist_test_images.npy").reshape(-1, 784)
+    train_values = np.load("small_mnist_train_labels.npy")
+    test_values  = np.load("small_mnist_test_labels.npy")
+    m3train, m3test = stochastic_gradient_descent(100, 100, 0.1, train_images, test_images, train_values, test_values)
