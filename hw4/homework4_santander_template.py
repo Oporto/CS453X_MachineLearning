ec2dd133d99aa79af62698a86d1b486cd1338015
mattmcd25 <mattmcd25@gmail.com>
2019-04-10 17:17:32 -0400

    start part 2

--- hw4/homework4_santander_template.py
+++ hw4/homework4_santander_template.py
@@ -9,13 +9,18 @@ y = np.array(d.target)  # Labels
 X = np.array(d.iloc[:,2:])  # Features
 
 # Split into train/test folds
+print(d.shape)
 # TODO
 
 # Linear SVM
 # TODO
+svm_lin = sklearn.svm.SVC(kernel='linear', C=1e15)
+svm_lin.fit(X, y)
 
 # Non-linear SVM (polynomial kernel)
 # TODO
+svm_poly = sklearn.svm.SVC(kernel='poly', degree=3, C=1e15)
+svm_poly.fit(X, y)
 
 # Apply the SVMs to the test set
 #yhat1 = ...  # Linear kernel
@@ -25,5 +30,6 @@ X = np.array(d.iloc[:,2:])  # Features
 #auc1 = ...
 #auc2 = ...
 
-print(auc1)
-print(auc2)
+# print(auc1)
+# print(auc2)
+
