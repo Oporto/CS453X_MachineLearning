dbdd09e01f2ce361fb6a7bc415e603cba1719565
mattmcd25 <mattmcd25@gmail.com>
2019-04-17 19:29:51 -0400

    plotting

--- hw5/homework5_pdevasconcelloso_mjmcdonald.py
+++ hw5/homework5_pdevasconcelloso_mjmcdonald.py
@@ -1,4 +1,9 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+def color(labels):
+    label = np.argmax(labels)+1
+    return '#'+hex(0x199999 * label)[2:]
 
 def PCA(X, y):
     X_mean = np.mean(X, axis = 1).reshape(-1,1)
@@ -8,8 +13,12 @@ def PCA(X, y):
     sorted_indices = np.argsort(vals)
     top_vs = vects[sorted_indices[-2:]]
     projection = top_vs.dot(X.transpose()).transpose()
+
+    for i, point in enumerate(projection):
+        plt.plot(point[0], point[1], color=color(y[i]), marker="o")
+    plt.show()
     
 if __name__ == "__main__":
     test_images = np.load("small_mnist_test_images.npy").reshape(-1, 784)
     test_values = np.load("small_mnist_test_labels.npy")
-    PCA(test_images[:100], test_values[:100])
+    PCA(test_images, test_values)
