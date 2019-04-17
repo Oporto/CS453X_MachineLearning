86e7437b50ed19d8913dfca4a27eef5da4cebff0
mattmcd25 <mattmcd25@gmail.com>
2019-04-17 19:50:53 -0400

    final

--- hw5/homework5_pdevasconcelloso_mjmcdonald.py
+++ hw5/homework5_pdevasconcelloso_mjmcdonald.py
@@ -10,13 +10,10 @@ def PCA(X, y):
     X_2 = np.zeros((784,784))
     for i in range(X.shape[0]):
         X_2 += (X[i,:].reshape(784,1) - X_mean).dot((X[i,:].reshape(784,1) - X_mean).T)
-
-    print(X_2.shape)
     vals, vects = np.linalg.eig(X_2)
-    print(vals.shape)
     sorted_indices = np.argsort(vals)
-    top_vs = vects[sorted_indices[-2:]]
-    projection = top_vs.dot(X.transpose()).transpose()
+    top_vs = vects[:,sorted_indices[-2:]]
+    projection = top_vs.T.dot(X.transpose()).transpose()
 
     for i, point in enumerate(projection):
         plt.plot(point[0], point[1], color=color(y[i]), marker="o")
