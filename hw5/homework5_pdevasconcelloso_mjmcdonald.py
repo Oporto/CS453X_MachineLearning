f606a98e7902215e3f508289d087d7790bae8122
Pedro Oporto <pdevasconcelloso@wpi.edu>
2019-04-17 22:50:29 +0000

    Done with creating projections

--- hw5/homework5_pdevasconcelloso_mjmcdonald.py
+++ hw5/homework5_pdevasconcelloso_mjmcdonald.py
@@ -3,16 +3,13 @@ import numpy as np
 def PCA(X, y):
     X_mean = np.mean(X, axis = 1).reshape(-1,1)
     X_s = X - X_mean
-    X_s2 = np.apply_along_axis(lambda x: x.reshape(28,28).dot(x.reshape(28,28).transpose()), 1, X_s).shape
+    X_s2 = X_s.transpose().dot(X_s)
     vals, vects = np.linalg.eig(X_s2)
-    top_indices = np.argsort(vals,1)
-    X_vs = []
-    for i in range(5000):
-        X_vs.append(vects[i][top_indices[i][-2:]])
-    X_vs = np.array(X_vs)
-    
-    projections = np.tensordot(X.reshape(-1,28,28), X_vs, axes=(1,2))
+    sorted_indices = np.argsort(vals)
+    top_vs = vects[sorted_indices[-2:]]
+    projection = top_vs.dot(X.transpose()).transpose()
     
 if __name__ == "__main__":
     test_images = np.load("small_mnist_test_images.npy").reshape(-1, 784)
     test_values = np.load("small_mnist_test_labels.npy")
+    PCA(test_images[:100], test_values[:100])
