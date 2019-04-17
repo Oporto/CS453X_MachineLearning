d2277167ea6ecc4057bea0de4ee3cc07a960c31c
Pedro Oporto <pdevasconcelloso@wpi.edu>
2019-04-17 23:29:58 +0000

    fixes

--- hw5/homework5_pdevasconcelloso_mjmcdonald.py
+++ hw5/homework5_pdevasconcelloso_mjmcdonald.py
@@ -1,10 +1,14 @@
 import numpy as np
 
 def PCA(X, y):
-    X_mean = np.mean(X, axis = 1).reshape(-1,1)
-    X_s = X - X_mean
-    X_s2 = X_s.transpose().dot(X_s)
-    vals, vects = np.linalg.eig(X_s2)
+    X_mean = np.mean(X, axis = 0).reshape(-1,1)
+    X_2 = np.zeros((784,784))
+    for i in range(X.shape[0]):
+        X_2 += (X[i,:].reshape(784,1) - X_mean).dot((X[i,:].reshape(784,1) - X_mean).T)
+
+    print(X_2.shape)
+    vals, vects = np.linalg.eig(X_2)
+    print(vals.shape)
     sorted_indices = np.argsort(vals)
     top_vs = vects[sorted_indices[-2:]]
     projection = top_vs.dot(X.transpose()).transpose()
